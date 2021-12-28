8d497d44952b56c510312a0c6081834204f57bd7
rhystic1 <rhysticone@gmail.com>
2021-12-28 20:52:13 +0000

    Add project files.

---
+++ FixSubAlignment/FixSub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FixSubAlignment
+{
+    public class FixSub : SelectedFolder
+    {
+        public static string? subFile = null;
+        public static void ChangeFileAlignment()
+        {
+            SelectFolder();
+            if(subFile != null)
+            {
+                string text = File.ReadAllText(subFile);
+                if (text.Contains("align:start"))
+                {
+                    text = text.Replace("align:start", "align:center");
+                    File.WriteAllText(subFile, text);
+                    MessageBox.Show("Done!",
+                        "Success!",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information,
+                        MessageBoxDefaultButton.Button1,
+                        MessageBoxOptions.ServiceNotification); // Gives focus to the message.
+                }
+                else
+                {
+                    MessageBox.Show("This subtitle file has already been modified.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ FixSubAlignment/SelectedFolder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FixSubAlignment
+{
+    public class SelectedFolder
+    {
+        public static void SelectFolder()
+        {
+            FixSub.subFile = default; // We reset the subFile argument every time to avoid an automatic else. Probably not the best way to do this but it works...
+            try
+            {
+                string text = File.ReadAllText(FixSub.subFile);
+            }
+            catch (ArgumentNullException)
+            {
+                OpenFileDialog b = new OpenFileDialog(); // We prompt the user for the correct folder and rerun the logic
+                b.Filter = "Subtitle File (*.vtt)|*.vtt";
+                if (b.ShowDialog() == DialogResult.OK)
+                {
+                    FixSub.subFile = b.FileName;
+                    return;
+                }
+                else if (b.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("You need to specify a valid subtitle file to continue.",
+                        "Error",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1, MessageBoxOptions.ServiceNotification);
+                }
+            }
+        }
+    }
+}
